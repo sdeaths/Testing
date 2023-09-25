bd1e2b7ec7cbdcb30c7dce14a2a413dc23174e99
Liza <urvanova03@mail.ru>
2023-09-25 14:46:30 +0300

    feat: added screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.testing"
-    compileSdk = 33
-
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.example.testing"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -59,6 +58,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+//    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
